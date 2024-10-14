e659c06b2a0753c4cd6c2d7b5e42b5e8195a306d
mg86837242 <mg86837242@protonmail.com>
2024-10-14 15:57:31 +0800

    Change IDs in seed data

--- api-server/src/main/java/dev/sy/electroneon/config/DatabaseSeeder.java
+++ api-server/src/main/java/dev/sy/electroneon/config/DatabaseSeeder.java
@@ -150,28 +150,28 @@ public class DatabaseSeeder {
     private void seedCarts(List<User> users, List<Product> products) {
         Cart cart1 = new Cart(
                 UUID.fromString(
-                        "51d5f27a-77c9-4fd0-993d-2e2c2c529a0b"),
+                        "31b7c3c0-83a4-42bb-97cc-de837b6015a7"),
                 users.getFirst(),
                 products.get(2),
                 1
         );
         Cart cart2 = new Cart(
                 UUID.fromString(
-                        "521421eb-4ca1-47eb-bb18-02188e0c4b6d"),
+                        "3283e51c-fe1a-4180-a03d-911181435336"),
                 users.getFirst(),
                 products.get(3),
                 2
         );
         Cart cart3 = new Cart(
                 UUID.fromString(
-                        "5331004b-ee52-4458-8fe9-d4bada3fd2be"),
+                        "33200d98-f9d3-48d1-b3e8-f87ee833f71f"),
                 users.get(1),
                 products.get(2),
                 3
         );
         Cart cart4 = new Cart(
                 UUID.fromString(
-                        "54360ebf-2a68-4dcc-b9d2-c12c38e1be21"),
+                        "3420cfa9-854d-4e53-b076-6d7493314c20"),
                 users.get(1),
                 products.get(3),
                 4
@@ -208,28 +208,28 @@ public class DatabaseSeeder {
     private void seedOrderProducts(List<Order> orders, List<Product> products) {
         OrderProduct orderProduct1 = new OrderProduct(
                 UUID.fromString(
-                        "31b7c3c0-83a4-42bb-97cc-de837b6015a7"),
+                        "51d5f27a-77c9-4fd0-993d-2e2c2c529a0b"),
                 orders.getFirst(),
                 products.getFirst(),
                 1
         );
         OrderProduct orderProduct2 = new OrderProduct(
                 UUID.fromString(
-                        "3283e51c-fe1a-4180-a03d-911181435336"),
+                        "521421eb-4ca1-47eb-bb18-02188e0c4b6d"),
                 orders.getFirst(),
                 products.get((1)),
                 2
         );
         OrderProduct orderProduct3 = new OrderProduct(
                 UUID.fromString(
-                        "33200d98-f9d3-48d1-b3e8-f87ee833f71f"),
+                        "5331004b-ee52-4458-8fe9-d4bada3fd2be"),
                 orders.get(1),
                 products.getFirst(),
                 3
         );
         OrderProduct orderProduct4 = new OrderProduct(
                 UUID.fromString(
-                        "3420cfa9-854d-4e53-b076-6d7493314c20"),
+                        "54360ebf-2a68-4dcc-b9d2-c12c38e1be21"),
                 orders.get(1),
                 products.get(1),
                 4
